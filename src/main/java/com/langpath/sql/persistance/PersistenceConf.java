d25ebd320eee1c956b41a7ff4ea82aadf7cf583c
Sebastian Jakowski <sebastian.jakowski@gft.com>
2016-03-17 08:36:21 +0100

    Add FindBug to gradle, Rename class

--- src/main/java/com/langpath/sql/persistance/PersistenceConf.java
+++ src/main/java/com/langpath/sql/persistance/PersistenceConf.java
@@ -19,7 +19,7 @@ import java.util.Properties;
  */
 @Configuration
 @EnableTransactionManagement
-public class PersistanceConf {
+public class PersistenceConf {
 
     @Bean
     public LocalContainerEntityManagerFactoryBean entityManagerFactory(){
