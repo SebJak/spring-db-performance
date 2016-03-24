e74a41ac81aa736f7ee2dd9ad2391fa229ac5713
sebjak <sjak90@gmail.com>
2016-03-24 22:51:04 +0100

    TDD userService

--- spring-sql-performance/src/test/java/com/langpath/service/impl/UserServiceTest.java
+++ spring-sql-performance/src/test/java/com/langpath/service/impl/UserServiceTest.java
@@ -1,27 +1,74 @@
 package com.langpath.service.impl;
 
-import static org.junit.Assert.*;
-
+import com.langpath.Application;
+import com.langpath.exceptions.FailLoginException;
+import com.langpath.exceptions.FailLogoutException;
+import com.langpath.sql.model.entity.user.User;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.Optional;
 
 /**
  * Created by Sebastian on 2016-03-18.
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(Application.class)
 public class UserServiceTest {
 
-    @Test
-    public void testLogin() throws Exception {
+    @Autowired
+    private UserService userService;
 
+    @Test(expected = FailLoginException.class)
+    public void testWrongCredentialLogin(){
+        userService.login("login", "password");
+    }
+
+    @Test(expected = FailLoginException.class)
+    public void testNullLogin() throws Exception {
+        userService.login(null, null);
+    }
+
+    @Test(expected = FailLoginException.class)
+    public void testNullNameLogin() throws Exception {
+        userService.login(null, "pass");
+    }
+
+    @Test(expected = FailLoginException.class)
+    public void testNullPassLogin() throws Exception {
+        userService.login("login", null);
+    }
+
+    @Test
+    public void testSecondLoginLogin() throws Exception {
+        userService.login("login", "password");
+        userService.login("login", "password");
+        //Need add check action on second user;
     }
 
     @Test
     public void testLogout() throws Exception {
+        Optional<User> user = userService.login("correctLogin", "correctPass");
+        userService.logout(user.get());
+    }
 
+    @Test(expected = FailLogoutException.class)
+    public void testLogoutNoLoggedUser() throws Exception {
+        User user = new User();
+        userService.logout(user);
+    }
+
+    @Test(expected = FailLogoutException.class)
+    public void testLogoutNullUser() throws Exception {
+        userService.logout(null);
     }
 
     @Test
     public void testSave() throws Exception {
-
+        User user = new User();
     }
 
     @Test
