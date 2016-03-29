4bb7dd718ad642a62c1f30e9599596de929b4abb
sebjak <sjak90@gmail.com>
2016-03-29 22:51:29 +0200

    Improve wordTest and word model

---
+++ spring-sql-performance/src/main/java/com/langpath/service/api/SecurityUserApi.java
@@ -0,0 +1,7 @@
+package com.langpath.service.api;
+
+/**
+ * Created by Sebastian on 2016-03-31.
+ */
+public class SecurityUserApi {
+}
---
+++ spring-sql-performance/src/main/java/com/langpath/service/impl/SecurityUserService.java
@@ -0,0 +1,7 @@
+package com.langpath.service.impl;
+
+/**
+ * Created by Sebastian on 2016-03-31.
+ */
+public class SecurityUserService {
+}
