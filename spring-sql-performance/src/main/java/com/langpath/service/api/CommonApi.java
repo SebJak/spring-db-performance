cd1811d301e7e4926e095adfa36c57e6094521ce
Sebastian Jakowski <sjak90@gmail.com>
2016-03-30 16:42:58 +0200

    add generic class. refactor commonApi

--- spring-sql-performance/src/main/java/com/langpath/service/api/CommonApi.java
+++ spring-sql-performance/src/main/java/com/langpath/service/api/CommonApi.java
@@ -1,22 +1,25 @@
 package com.langpath.service.api;
 
+import com.langpath.data.model.entity.base.BaseEntity;
+
 import java.util.Collection;
 import java.util.Optional;
 
 /**
  * Created by Sebastian on 2016-03-18.
  */
-public interface CommonApi<K,T> {
-    Optional<K> save(K word);
+public interface CommonApi<K extends BaseEntity,T> {
 
-    Optional<Collection<K>> save(Iterable<K> word);
+    Optional<K> save(K entity);
 
-    Optional<K> update(K word);
+    Optional<Collection<K>> save(Iterable<K> entities);
+
+    Optional<K> update(K entity);
 
     Optional<K> findById(T id);
 
     Optional<Collection<K>> findAll();
 
-    Boolean remove(K word);
+    Boolean remove(K entity);
 
 }
---
+++ spring-sql-performance/src/test/java/com/langpath/service/impl/GenericCrudTests.java
@@ -0,0 +1,48 @@
+package com.langpath.service.impl;
+
+import static org.junit.Assert.*;
+
+import com.langpath.data.model.entity.base.BaseEntity;
+import com.langpath.service.api.CommonApi;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Optional;
+
+/**
+ * Created by sjakowski on 2016-03-30.
+ */
+public abstract class GenericCrudTests<T extends BaseEntity, E extends CommonApi> {
+
+    @Autowired
+    private E service;
+
+    abstract T getCorrectObjectToSave();
+
+    @Test
+    public void testSave() throws Exception {
+        T entity = getCorrectObjectToSave();
+        Optional<T> saved = service.save(entity);
+        assertEquals("Should be present", saved.isPresent());
+    }
+
+    @Test
+    public void testUpdate() throws Exception {
+
+    }
+
+    @Test
+    public void testFindById() throws Exception {
+
+    }
+
+    @Test
+    public void testFindAll() throws Exception {
+
+    }
+
+    @Test
+    public void testRemove() throws Exception {
+
+    }
+}
